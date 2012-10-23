e7a57c18baa6022ee5e7e4be83a4b78ba084ccb3
Necromos <masternecromos@gmail.com>
2012-10-23 20:02:33 +0200

    template z 23.10.2012

---
+++ rozklad_jazdy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RozkladJazdy::Application.config.secret_token = '74f1ce492af3e6889539640bf938b0da001c5df8368828e7dc885ebddac77ac80fbd966492b5536653ab16675a5674f35de71e705a09f39b2adbff3ab9571741'
