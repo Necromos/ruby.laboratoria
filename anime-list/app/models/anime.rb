fe683389da6b14978e73fcdf9ccaf68620cb712c
Necromos <masternecromos@gmail.com>
2012-11-11 19:32:06 +0100

    dopiska att w Anime

--- anime-list/app/models/anime.rb
+++ anime-list/app/models/anime.rb
@@ -1,5 +1,5 @@
 class Anime < ActiveRecord::Base
-  attr_accessible :noe, :title, :yop
+  attr_accessible :noe, :title, :yop, :doc, :doe
   validates :noe, :length => { :minimum => 2, :maximum => 3}, :numericality => true, :presence => true
   validates :title, :uniqueness => true, :presence => true
   validates :yop, :inclusion => { :in => 1900...2012}, :numericality => true
