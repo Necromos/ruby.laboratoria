b3536c318bd45848ce0ed0c2d4312dabc13b30f3
Necromos <masternecromos@gmail.com>
2012-11-11 18:46:55 +0100

    1sza tura walidatorów

--- anime-list/app/models/anime.rb
+++ anime-list/app/models/anime.rb
@@ -1,3 +1,6 @@
 class Anime < ActiveRecord::Base
   attr_accessible :noe, :title, :yop
+  validates :noe, :length => { :minimum => 2, :maximum => 3}, :numericality => true, :presence => true
+  validates :title, :uniqueness => true, :presence => true
+  validates :yop, :inclusion => { :in => 1900...2012}, :numericality => true
 end
